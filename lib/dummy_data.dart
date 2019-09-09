afef6ed250a4e453c9584ecbbd399bef4a968fed
Mugi <mugypleci@gmail.com>
2019-09-09 17:46:49 +0700

    add data dummy

---
+++ lib/dummy_data.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:multiple_screens/models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: '01',
+    title: 'Italian',
+    color: Colors.purple
+  ),
+   Category(
+    id: '02',
+    title: 'Pizza',
+    color: Colors.pink
+  ),
+   Category(
+    id: '02',
+    title: 'Hamburgers',
+    color: Colors.green
+  ),
+   Category(
+    id: '03',
+    title: 'Donuts',
+    color: Colors.yellow
+  ),
+   Category(
+    id: '04',
+    title: 'J&Co',
+    color: Colors.grey
+  ),
+   Category(
+    id: '05',
+    title: 'French Frise',
+    color: Colors.blue
+  ),
+   Category(
+    id: '06',
+    title: 'Rice Box',
+    color: Colors.brown
+  ),
+   Category(
+    id: '07',
+    title: 'Hot Dog',
+    color: Colors.green
+  ),
+   Category(
+    id: '08',
+    title: 'Fred Chicken',
+    color: Colors.teal
+  ),
+  Category(
+    id: '09',
+    title: 'Quick & Easy',
+    color: Colors.red
+  ),
+   Category(
+    id: '10',
+    title: 'Ice Land',
+    color: Colors.lime
+  ),
+];
