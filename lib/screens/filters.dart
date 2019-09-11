bee978cdbec312271b56f0c9254bcf86538b33af
Mugi <mugypleci@gmail.com>
2019-09-11 22:22:13 +0700

    navigation replacing pages

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
    static const routeName = 'Filters';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filters'),
+      ),
+      drawer: MainDrawer(),
+      body: Text('Favorites'),
     );
   }
 }
